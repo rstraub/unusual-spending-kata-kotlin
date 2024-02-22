7f3e6491926aa90f1dc4765ed87576eabb4cff1b
Roy Straub <rstraub@xebia.com>
2024-02-22 14:06:50 +0100

    Better variable names

--- src/main/kotlin/nl/codecraftr/kata/unusualspending/Detector.kt
+++ src/main/kotlin/nl/codecraftr/kata/unusualspending/Detector.kt
@@ -2,17 +2,19 @@ package nl.codecraftr.kata.unusualspending
 
 object Detector {
     fun detect(prevMonth: List<Payment>, currentMonth: List<Payment>): List<CategorySpending> {
-        val groupedPrevMonth = toSpendingPerCategory(prevMonth)
-        val groupedCurrentMonth = toSpendingPerCategory(currentMonth)
+        val totalSpentPerCategoryPrevMonth = calculateSpendingPerCategory(prevMonth)
+        val totalSpentPerCategoryCurrentMonth = calculateSpendingPerCategory(currentMonth)
 
-        return groupedPrevMonth.zip(groupedCurrentMonth)
+        return totalSpentPerCategoryPrevMonth.zip(totalSpentPerCategoryCurrentMonth)
                 .filter { isUnusual(it.first, it.second) }
                 .map { it.second }
     }
 
-    private fun toSpendingPerCategory(payments: List<Payment>) = payments.groupBy { it.category }.mapValues {
-        CategorySpending(it.value.sumOf { p -> p.amount }, it.key)
+    private fun calculateSpendingPerCategory(payments: List<Payment>) = payments.groupBy(Payment::category).mapValues {
+        CategorySpending(calculateTotalAmount(it.value), it.key)
     }.values.toList()
+
+    private fun calculateTotalAmount(payments: List<Payment>) = payments.sumOf(Payment::amount)
 
     private fun isUnusual(prev: CategorySpending, current: CategorySpending) =
             prev.amount * 1.5 <= current.amount
