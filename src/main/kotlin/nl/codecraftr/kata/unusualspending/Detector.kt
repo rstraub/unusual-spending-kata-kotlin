0df667cde1283d208ed262e223f7ba67dee4a743
Roy Straub <rstraub@xebia.com>
2024-02-22 13:22:59 +0100

    Flag a single unsual spending

--- src/main/kotlin/nl/codecraftr/kata/unusualspending/Detector.kt
+++ src/main/kotlin/nl/codecraftr/kata/unusualspending/Detector.kt
@@ -1,8 +1,16 @@
 package nl.codecraftr.kata.unusualspending
 
 object Detector {
-    fun detect(prevMonth: List<Payment>, currentMonth: List<Payment>): Pair<Int, Category> {
-        return 15 to Category.Groceries
+    fun detect(prevMonth: List<Payment>, currentMonth: List<Payment>): List<Pair<Int, Category>> {
+        val prev = prevMonth.first()
+        val current = currentMonth.first()
+
+        return if (isUnusual(prev, current))
+            listOf(current.amount to current.category)
+        else
+            emptyList()
     }
 
+    private fun isUnusual(prev: Payment, current: Payment) =
+            prev.amount * 1.5 <= current.amount
 }
--- src/test/kotlin/nl/codecraftr/kata/unusualspending/DetectorTest.kt
+++ src/test/kotlin/nl/codecraftr/kata/unusualspending/DetectorTest.kt
@@ -2,16 +2,29 @@ package nl.codecraftr.kata.unusualspending
 
 import io.kotest.matchers.shouldBe
 import nl.codecraftr.kata.unusualspending.Category.Groceries
+import nl.codecraftr.kata.unusualspending.Category.Travel
 import org.junit.jupiter.api.Test
 
 class DetectorTest {
     @Test
-    fun `should detect unusual spending given over 50% increase within category`() {
+    fun `should flag unusual spending given over 50% increase within category`() {
         val prevMonth = listOf(Payment(10, "test", Groceries))
         val currentMonth = listOf(Payment(15, "test", Groceries))
 
         val result = Detector.detect(prevMonth, currentMonth)
 
-        result shouldBe Pair(15, Groceries)
+        result shouldBe listOf(15 to Groceries)
+    }
+
+    @Test
+    fun `should not flag spending given under 50% increase within category`() {
+        val prevMonth = listOf(
+                Payment(10, "test", Groceries))
+        val currentMonth = listOf(
+                Payment(14, "test", Groceries))
+
+        val result = Detector.detect(prevMonth, currentMonth)
+
+        result shouldBe listOf()
     }
 }
